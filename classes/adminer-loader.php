ccc8a08d7f11a60b89038602a1ffbf3896dafe77
skydiver <skydiver@users.noreply.github.com>
2016-06-16 17:01:22 -0300

    1.2.6: Fixed SQLite support

--- classes/adminer-loader.php
+++ classes/adminer-loader.php
@@ -1,42 +1,16 @@
 <?php
 
-    if(!function_exists('adminer_object')) {
+    # GET CONNECTION
+    $driver = config('database.default');
 
-        function adminer_object() {
-
-            require_once 'adminer-plugins/plugin.php';
-
-            if(is_array(Session::get('ADMINER_PLUGINS'))) {
-                foreach(Session::get('ADMINER_PLUGINS') as $plugin => $class) {
-                    require_once 'adminer-plugins/'.$plugin.'.php';
-                    $load_plugins[] = new $class;
-                }
-            }
-
-            class AdminerCustomization extends AdminerPlugin {
-
-                function name() {
-                    return Lang::get('martin.adminer::lang.plugin.name');
-                }
-
-                function credentials() {
-                    if(Session::get('ADMINER_AUTOLOGIN') === true) {
-                        require_once(plugins_path() . '/martin/adminer/classes/OctoberAdminerHelper.php');
-                        $connection = Martin\Adminer\Classes\OctoberAdminerHelper::getAutologinParams();
-                        if($connection['driver'] == 'mysql') {
-                            return [$connection['server'], $connection['username'], $connection['password']];
-                        }
-                    } else {
-                        return array(SERVER, $_GET['username'], get_password());
-                    }
-                }
-
-            }
-
-            return new AdminerCustomization($load_plugins);
-
-        }
-
+    # LOAD "adminer_object" FOR SPECIFIC DRIVER
+    switch($driver) {
+        case 'mysql':
+            require_once(plugins_path() . '/martin/adminer/classes/extra/MySQL.php');
+            break;
+        case 'sqlite':
+            require_once(plugins_path() . '/martin/adminer/classes/extra/SQLite.php');
+            break;
     }
 
     # LOAD MAIN ADMINER CLASS

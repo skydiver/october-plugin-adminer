394e3cef599156b414b2de9eb727a515945721cc
Martin <skydiver@users.noreply.github.com>
2018-08-05 01:02:56 -0300

    BETA: PHP linting

--- classes/adminer-loader.php
+++ classes/adminer-loader.php
@@ -1,38 +1,38 @@
 <?php
 
-    if(!function_exists('adminer_object')) {
+if (!function_exists('adminer_object')) {
 
-        function adminer_object() {
+    function adminer_object() {
 
-            require_once 'adminer-plugins/plugin.php';
+        include_once 'adminer-plugins/plugin.php';
 
-            if(is_array(Session::get('ADMINER_PLUGINS'))) {
-                foreach(Session::get('ADMINER_PLUGINS') as $plugin => $class) {
-                    require_once 'adminer-plugins/'.$plugin.'.php';
-                    $load_plugins[] = new $class;
-                }
+        if (is_array(Session::get('ADMINER_PLUGINS'))) {
+            foreach (Session::get('ADMINER_PLUGINS') as $plugin => $class) {
+                include_once 'adminer-plugins/'.$plugin.'.php';
+                $load_plugins[] = new $class;
             }
+        }
 
-            $driver = config('database.default');
+        $driver = config('database.default');
 
-            # LOAD ADMINER CUSTOMIZATION CLASS
-            switch($driver) {
-                case 'mysql':
-                    require_once(plugins_path() . '/martin/adminer/classes/customizations/MySQL.php');
-                    return new AdminerCustomization($load_plugins);
-                case 'pgsql':
-                    require_once(plugins_path() . '/martin/adminer/classes/customizations/pgSQL.php');
-                    return new AdminerCustomization($load_plugins);
-                case 'sqlite':
-                    require_once(plugins_path() . '/martin/adminer/classes/customizations/SQLite.php');
-                    return new AdminerCustomization($load_plugins);
-            }
-
+        // LOAD ADMINER CUSTOMIZATION CLASS
+        switch($driver) {
+            case 'mysql':
+                include_once plugins_path() . '/martin/adminer/classes/customizations/MySQL.php';
+                return new AdminerCustomization($load_plugins);
+            case 'pgsql':
+                include_once plugins_path() . '/martin/adminer/classes/customizations/pgSQL.php';
+                return new AdminerCustomization($load_plugins);
+            case 'sqlite':
+                include_once plugins_path() . '/martin/adminer/classes/customizations/SQLite.php';
+                return new AdminerCustomization($load_plugins);
         }
 
     }
 
-    # LOAD MAIN ADMINER CLASS
-    require(plugins_path() . '/martin/adminer/classes/adminer-4.6.2-en.php');
+}
+
+// LOAD MAIN ADMINER CLASS
+require plugins_path() . '/martin/adminer/classes/adminer-4.6.2-en.php';
 
 ?>
--- controllers/OctoberAdminer.php
+++ controllers/OctoberAdminer.php
@@ -1,68 +1,72 @@
 <?php
 
-    namespace Martin\Adminer\Controllers;
+namespace Martin\Adminer\Controllers;
 
-    use Config;
-    use Session;
-    use Backend\Classes\Controller;
-    use Martin\Adminer\Models\Settings as Settings;
-    use Martin\Adminer\Classes\OctoberAdminerHelper;
+use Config;
+use Session;
+use Backend\Classes\Controller;
+use Martin\Adminer\Models\Settings as Settings;
+use Martin\Adminer\Classes\OctoberAdminerHelper;
 
-    class OctoberAdminer extends Controller {
+class OctoberAdminer extends Controller {
 
-        public $pageTitle           = 'martin.adminer::lang.plugin.name';
-        public $requiredPermissions = ['martin.adminer.access_adminer'];
+    public $pageTitle           = 'martin.adminer::lang.plugin.name';
+    public $requiredPermissions = ['martin.adminer.access_adminer'];
 
-        public function __construct() {
-            Config::set('cms.enableCsrfProtection', false);
-            parent::__construct();
-            \BackendMenu::setContext('Martin.Adminer', 'adminer', 'octoberadminer');
-        }
-
-        public function index() {
-            $mode = Settings::get('mode', 1);
-            switch ($mode) {
-                case 2:
-                    return $this->runAdminerIFrame();
-                    break;
-                default:
-                    return $this->runAdminer();
-                    break;
-            }
-        }
-
-        public function iframe() {
-            return $this->runAdminer();
-        }
-
-        private function runAdminer() {
-            $this->runAdminerLoader();
-            define('ADMINER_THEME', self::getTheme());
-            require(plugins_path() . '/martin/adminer/classes/adminer-loader.php');
-            return new \Martin\Adminer\Classes\EmptyResponse();
-        }
-
-        private function runAdminerLoader() {
-            $mode      = Settings::get('mode'     , 1);
-            $autologin = Settings::get('autologin', 0);
-            if($mode       == 2) { Session::flash('ADMINER_PLUGINS', ['frames' => 'AdminerFrames']); }
-            if($autologin !== 0) { Session::flash('ADMINER_AUTOLOGIN', true); }
-        }
-
-        private function runAdminerIFrame() {
-            $this->addCss('/plugins/martin/adminer/assets/css/styles.css');
-            return \View::make('martin.adminer::iframe', [
-                'URL' => \Backend::url('martin/adminer/octoberadminer/iframe' . OctoberAdminerHelper::getAutologinURL())
-            ]);
-        }
-
-        private function getTheme() {
-            if(Settings::get('theme')) {
-                $css = '/plugins/martin/adminer/assets/themes/' . Settings::get('theme') . '/adminer.css?cache=' . date('YmdHis');
-                return url($css);
-            }
-        }
-
+    public function __construct() {
+        Config::set('cms.enableCsrfProtection', false);
+        parent::__construct();
+        \BackendMenu::setContext('Martin.Adminer', 'adminer', 'octoberadminer');
     }
+
+    public function index() {
+        $mode = Settings::get('mode', 1);
+        switch ($mode) {
+            case 2:
+                return $this->runAdminerIFrame();
+                break;
+            default:
+                return $this->runAdminer();
+                break;
+        }
+    }
+
+    public function iframe() {
+        return $this->runAdminer();
+    }
+
+    private function runAdminer() {
+        $this->runAdminerLoader();
+        define('ADMINER_THEME', self::getTheme());
+        include_once plugins_path() . '/martin/adminer/classes/adminer-loader.php';
+        return new \Martin\Adminer\Classes\EmptyResponse();
+    }
+
+    private function runAdminerLoader() {
+        $mode      = Settings::get('mode', 1);
+        $autologin = Settings::get('autologin', 0);
+        if ($mode == 2) {
+            Session::flash('ADMINER_PLUGINS', ['frames' => 'AdminerFrames']);
+        }
+        if ($autologin !== 0) {
+            Session::flash('ADMINER_AUTOLOGIN', true);
+        }
+    }
+
+    private function runAdminerIFrame() {
+        $this->addCss('/plugins/martin/adminer/assets/css/styles.css');
+        return \View::make('martin.adminer::iframe', [
+            'URL' => \Backend::url('martin/adminer/octoberadminer/iframe' . OctoberAdminerHelper::getAutologinURL())
+        ]);
+    }
+
+    private function getTheme() {
+        if (Settings::get('theme')) {
+            $css = '/plugins/martin/adminer/assets/themes/' . Settings::get('theme') . '/adminer.css?cache=' . date('YmdHis');
+            return url($css);
+        }
+    }
+
+}
 
 ?>
